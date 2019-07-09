52152de23804318c7890dd3d204b9111d0518af8
jacobwfinn <jacobfinn155@hotmail.com>
2019-07-08 20:09:06 -0400

    Changed to Input Fields

--- Assets/Scripts/Holder.cs
+++ Assets/Scripts/Holder.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Holder : MonoBehaviour
 {
-    public int sizeX, sizeY;
-    public float density;
-    public bool randomized;
+    public InputField textX, textY;
+    public Toggle isRandom;
+    public Slider dense;
+
+    int sizeX, sizeY;
+    float density;
+    bool randomized;
 
     // Start is called before the first frame update
     void Start()
